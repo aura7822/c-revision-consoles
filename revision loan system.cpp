cd3dce81fc5453b073fc41cc4a7e65fb826e3c89
aura <joshuaura7822@gmail.com>
2025-04-19 20:33:49 +0300

    bank loan system to perfection

---
+++ revision loan system.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int id;
+int date;
+
+int main(){
+    cout<<"Baok ID : ";
+    cin>>id;
+    cout<<"Due date : ";
+    cin>>date;
+    cout<<"Return date : "
+    cin>>date;
+
+    return 0;
+}
+
